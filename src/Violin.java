9df228cc5635803edf7ee35667ec1e4df5e76b98
Milana--proger <mmilya06@mail.ru>
2026-02-20 19:51:25 +0300

    Added method in Violin
    Signed-off-by: Milana--proger <mmilya06@mail.ru>

--- src/Violin.java
+++ src/Violin.java
@@ -1,4 +1,9 @@
 public class Violin extends MusicalInstrument{
     int volume;
     String material;
+
+    public String getMaterial() {
+        System.out.println("В процессе получения материала");
+        return "Получен материал";
+    }
 }
