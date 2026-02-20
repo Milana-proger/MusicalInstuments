24b95de3ac278689d9f9f47777a42a4d8feb7de3
Milana--proger <mmilya06@mail.ru>
2026-02-20 19:25:32 +0300

    Creation of MusicalInstruments
    Signed-off-by: Milana--proger <mmilya06@mail.ru>

---
+++ src/MusicalInstrument.java
@@ -0,0 +1,18 @@
+public class MusicalInstrument {
+    String name;
+    double weight;
+    String colour;
+    double volume;
+
+    public void playing() {
+        System.out.println("Инструмент играет");
+    }
+
+    public void adjusting() {
+        System.out.println("Происходит настройка инструмента");
+    }
+
+    public void existing() {
+        System.out.println("Инструмент ждет своего часа");
+    }
+}
